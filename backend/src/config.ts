ec7167bf8dee2d8a00c5cd8f99d819c9288ad615
Benjamin Harvey <benjaminharvey2000@gmail.com>
2026-04-05 16:59:13 +0930

    Auth0 configured

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -3,7 +3,9 @@ const config = {
     port: parseInt(process.env.PORT || "5000"),
     debug: process.env.APP_DEBUG === "true",
     jwtSecret: process.env.JWT_SECRET || "",
-    appName: process.env.APP_NAME || ""
+    appName: process.env.APP_NAME || "",
+    issueBaseUrl: process.env.ISSUER_BASE_URL || "",
+    audience: process.env.AUDIENCE || ""
 };
 
 export default config;
--- backend/src/middleware/authenticate-user.ts
+++ backend/src/middleware/authenticate-user.ts
@@ -1,30 +1,9 @@
-import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken"
-import AuthenticationError from "../errors/AuthenticationError"
+import { auth } from "express-oauth2-jwt-bearer";
 import config from "../config";
 
-export default function authenticateUser(req: Request, res: Response, next: NextFunction) {
+const authenticateUser = auth({
+    audience: config.audience,
+    issuerBaseURL: config.issueBaseUrl
+});
 
-    const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith("Bearer ")) {
-        throw new AuthenticationError({
-            message: "Authorisation header missing or malformed",
-            statusCode: 401,
-            code: "ERR_AUTH"
-        })
-    }
-
-    const token = authHeader.split(" ")[1];
-
-    try {
-        const decoded = jwt.verify(token, config.jwtSecret);
-        req.auth = { payload: decoded as JwtPayload, token };
-        next();
-    } catch (error) {
-        throw new AuthenticationError({
-            message: "You are not authorised to perform this operation",
-            statusCode: 403,
-            code: "ERR_AUTH"
-        })
-    }
-}
+export default authenticateUser
